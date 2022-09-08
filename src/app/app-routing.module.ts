0e559c3a3a58e11d3db8879ff7e6a833ac4eb44b
Kaue-Dias-Lipari <kauelipari@uol.com.br>
2022-09-07 23:38:05 -0300

    add component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginaContatoComponent } from './pagina-contato/pagina-contato.component';
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PaginaInicialComponent},
+  {path: 'contato', component: PaginaContatoComponent}
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
+import { PaginaContatoComponent } from './pagina-contato/pagina-contato.component';
+import { TecnologiasComponent } from './tecnologias/tecnologias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PaginaInicialComponent,
+    PaginaContatoComponent,
+    TecnologiasComponent
   ],
   imports: [
     BrowserModule,
